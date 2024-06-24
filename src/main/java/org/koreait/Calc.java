ac6517d82d332e621e2223343df704ddb10b3923
s3496h <s3496h@gmail.com>
2024-06-24 23:54:35 +0900

    10 - 20 + 30

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -2,14 +2,17 @@ package org.koreait;
 
 public class Calc {
     public static int run(String exp) {
-        boolean needToPlus = exp.contains("+");
-        boolean needToMinus = exp.contains("-");
+        System.out.println(exp);
+
+        exp = exp.replaceAll("- ", "+ -");
+
+        System.out.println(exp);
+
         String[] bits = null;
-        if (needToPlus) {
+
             bits = exp.split(" \\+ ");
-        } else if (needToMinus) {
-            bits = exp.split(" - ");
-        }
+
+
 
 
         int a = Integer.parseInt(bits[0]);
@@ -17,15 +20,12 @@ public class Calc {
         int c = 0;
 
         if (bits.length > 2) {
-         c = Integer.parseInt(bits[2]);
+            c = Integer.parseInt(bits[2]);
         }
 
-        if (needToPlus) {
-            return a + b + c;
-        } else if (needToMinus) {
-            return a - b - c;
+    return a + b + c;
         }
-        throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
+      //  throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
     }
-}
+
 
