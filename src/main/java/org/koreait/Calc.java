a13afea88e3525e6f069566d077fb9e20f67610c
ãs3496h <s3496h@gmail.com>
2024-06-25 13:58:54 +0900

    10 * 20 + 10 + 5 * 2

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -1,18 +1,35 @@
 package org.koreait;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Calc {
     public static int run(String exp) {
 
+        if (!exp.contains(" ")) {
+            return Integer.parseInt(exp);
+        }
+
         boolean needToMulti = exp.contains("*");
         boolean needToPlus = exp.contains("+");
-         boolean needToCompound = needToMulti && needToPlus;
 
-         if (needToCompound) {
-             String[] bits  = exp.split(" \\+ ");
-             return Integer.parseInt(bits[0]) + run(bits[1]);
-         }
-         if (needToPlus) {
-           exp = exp.replace("- ", "- +");
+        boolean needToCompound = needToMulti && needToPlus;
+
+
+        if (needToCompound) {
+            String[] bits = exp.split(" \\+ ");
+
+            //단일항이 들어오면 바로 리턴
+            String newExp = Arrays.stream(bits)
+
+                    .mapToInt(Calc::run)
+                    .mapToObj(e -> e + "")
+                    //객체가 아닌데 객체로 바꿔야할 때
+                    .collect(Collectors.joining(" + "));
+            return run(newExp);
+        }
+        if (needToPlus) {
+            exp = exp.replace("- ", "- +");
             String[] bits = exp.split(" \\+ ");
 
             int sum = 0;
@@ -34,8 +51,7 @@ public class Calc {
             }
 
             return sum;
-        }
-        else
-        throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
+        } else
+            throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
     }
 }
